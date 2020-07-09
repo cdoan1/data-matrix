764ae32efa222d28ac8c6f8c24d1e6e6ba33101c
Chris Doan <cdoan@redhat.com>
2020-07-09 15:42:50 -0500

    add filter examples

--- parse-data.py
+++ parse-data.py
@@ -5,9 +5,12 @@ df = pd.read_csv("./version-v-tested.csv")
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
 
-sorting = df.sort_values(by=['release'], ascending=False)
+# filter out any failing rows
+passing = df[df['result'].str.contains("pass")]
+
+sorting = passing.sort_values(by=['release'], ascending=False)
 print(df.head())
 print(sorting.head())
 
-release43 = df[df['release'].str.contains("4.3")].sort_values(by=['release'], ascending=False)
+release43 = passing[passing['release'].str.contains("4.3")].sort_values(by=['release'], ascending=False)
 print(release43.head())
